bb7e5c098d2f9064908eb37c8c7e96f815c4dffb
ivanjocc <ivanjose_04@hotmail.com>
2025-01-03 14:20:37 -0500

    docker, routes with docker

--- front/health-surveillance/src/App.jsx
+++ front/health-surveillance/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>404: Page Not Found</div>} />
         <Route
           path="/dashboard"
           element={
